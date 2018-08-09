0b4f2ff14c0bbc9acf43eeceb26441e9181f8970
ferchichi mohamed kacem <kassem.ferchichi@gmail.com>
2018-08-09 09:27:57 +0000

    split view Done

--- webapp/controller/Requester.controller.js
+++ webapp/controller/Requester.controller.js
@@ -2,43 +2,18 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function (Controller) {
 	"use strict";
-
+	
 	return Controller.extend("nextgen.nextgen.controller.RequesterView", {
-
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf nextgen.nextgen.view.RequesterView
-		 */
-		//	onInit: function() {
-		//
-		//	},
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf nextgen.nextgen.view.RequesterView
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf nextgen.nextgen.view.RequesterView
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf nextgen.nextgen.view.RequesterView
-		 */
-		//	onExit: function() {
-		//
-		//	}
+		
+         onMultiSelectPress: function(oEvent) {
+		if (oEvent.getSource().getPressed()) {
+			sap.m.MessageToast.show("MultiSelect Pressed");
+		} else {
+			sap.m.MessageToast.show("MultiSelect Unpressed");
+		}
+	}
+    
+	
 
 	});
 
